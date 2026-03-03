9d9ff7f39b3f1eced048701a344f31e10d15e99b
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2026-03-03 17:42:04 +0100

    Refactor, plus added more tests

--- MattSourceGenHelpers.Abstractions/Generate.cs
+++ MattSourceGenHelpers.Abstractions/Generate.cs
@@ -2,7 +2,7 @@
 
 public static class Generate
 {
-    public static IGeneratorsFactory CurrentGenerator { get; set; } = new RecordingGeneratorsFactory();
+    internal static IGeneratorsFactory CurrentGenerator { get; set; } = new RecordingGeneratorsFactory();
 
     public static IMethodBuilder Method() => new MethodBuilder(CurrentGenerator);
 }
@@ -18,61 +18,20 @@ public interface IMethodBuilder<TArg1>
     IMethodImplementationGenerator<TArg1, TReturnType> WithReturnType<TReturnType>();
 }
 
-public class MethodBuilder : IMethodBuilder
+public class MethodBuilder(IGeneratorsFactory generatorsFactory) : IMethodBuilder
 {
-    private readonly IGeneratorsFactory _generatorsFactory;
+    public IMethodBuilder<TArg1> WithParameter<TArg1>() => new MethodBuilder<TArg1>(generatorsFactory);
 
-    public MethodBuilder(IGeneratorsFactory generatorsFactory)
-    {
-        _generatorsFactory = generatorsFactory;
-    }
-
-    public IMethodBuilder<TArg1> WithParameter<TArg1>() => new MethodBuilder<TArg1>(_generatorsFactory);
-
-    public IMethodImplementationGenerator<TReturnType> WithReturnType<TReturnType>() => _generatorsFactory.CreateImplementation<TReturnType>();
+    public IMethodImplementationGenerator<TReturnType> WithReturnType<TReturnType>() => generatorsFactory.CreateImplementation<TReturnType>();
 }
 
-public class MethodBuilder<TArg1> : IMethodBuilder<TArg1>
+public class MethodBuilder<TArg1>(IGeneratorsFactory generatorsFactory) : IMethodBuilder<TArg1>
 {
-    private readonly IGeneratorsFactory _generatorsFactory;
-
-    public MethodBuilder(IGeneratorsFactory generatorsFactory)
-    {
-        _generatorsFactory = generatorsFactory;
-    }
-
-    public IMethodImplementationGenerator<TArg1, TReturnType> WithReturnType<TReturnType>() => _generatorsFactory.CreateImplementation<TArg1, TReturnType>();
-}
-
-public class EmptyGeneratorsFactory : IGeneratorsFactory
-{
-    public IMethodImplementationGenerator CreateImplementation() => new EmptyMethodImplementationGenerator();
-    public IMethodImplementationGenerator<TReturnType> CreateImplementation<TReturnType>() => new EmptyMethodImplementationGenerator<TReturnType>();
-    public IMethodImplementationGenerator<TArg1, TReturnType> CreateImplementation<TArg1, TReturnType>() => new EmptyMethodImplementationGenerator<TArg1, TReturnType>();
-}
-
-public class EmptyMethodImplementationGenerator : IMethodImplementationGenerator
-{
-    public IMethodImplementationGenerator WithBody(Action body) => this;
-    public IMethodImplementationGenerator WithBody(Func<object> body) => this;
-}
-
-public class EmptyMethodImplementationGenerator<TReturnType> : IMethodImplementationGenerator<TReturnType>
-{
-    public IMethodImplementationGenerator UseBody(Func<object> body) => this;
-}
-
-public class EmptyMethodImplementationGenerator<TArg1, TReturnType> : IMethodImplementationGenerator<TArg1, TReturnType>
-{
-    public IMethodImplementationGeneratorSwitchBody<TArg1, TReturnType> WithSwitchBody() =>
-        new RecordingMethodImplementationGeneratorSwitchBody<TArg1, TReturnType>(new SwitchBodyRecord());
-    public IMethodImplementationGenerator WithBody(Action body) => this;
-    public IMethodImplementationGenerator WithBody(Func<object> body) => this;
+    public IMethodImplementationGenerator<TArg1, TReturnType> WithReturnType<TReturnType>() => generatorsFactory.CreateImplementation<TArg1, TReturnType>();
 }
 
 public interface IGeneratorsFactory
 {
-    IMethodImplementationGenerator CreateImplementation();
     IMethodImplementationGenerator<TReturnType> CreateImplementation<TReturnType>();
     IMethodImplementationGenerator<TArg1, TReturnType> CreateImplementation<TArg1, TReturnType>();
 }
